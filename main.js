ce0cff50a7dc1e485f0d91a7d757e5cc8e0911dc
mxaddict <mxaddict@kryptic.sh>
2025-04-10 06:52:32 +0800

    You might not think that programmers are artists, but programming is an extremely creative profession. It's logic-based creativity. - John Romero

--- main.js
+++ main.js
@@ -22,23 +22,16 @@ const log = ololog.configure({
 const env = process.env;
 const apikey = env.APIKEY || "";
 const secret = env.SECRET || "";
-const exchange = env.EXCHANGE || "binance";
-const asset = env.ASSET || "FLOKI";
+const asset = env.ASSET || "BTC";
 const base = env.BASE || "USDT";
-const fee = env.FEE || "BNB";
-const ordersMax = env.ORDERS_MAX || 2;
-const ordersAmountPercent = env.ORDERS_AMOUNT_PERCENT || 0.05;
-const spreadPercent = env.SPREAD_PERCENT || 0.003;
+const ordersMax = env.ORDERS_MAX || 4;
+const ordersAmountPercent = env.ORDERS_AMOUNT_PERCENT || 0.1;
+const spreadPercent = env.SPREAD_PERCENT || 0.002;
 
-const updateTicks = 5;
-const tickerTicks = 10;
 const symbol = `${asset}/${base}`;
-const feeRequiredPercent = 0.1;
-const feeSymbol = `${fee}/${base}`;
+const updateTicks = 5;
 const failInterval = 5 * 1000;
-const updateInterval = 5 * 1000;
 const decimals = 2;
-const sats = 8;
 
 const dataFile = ".data.json";
 
@@ -81,22 +74,22 @@ while (true) {
 	}
 
 	try {
-		const ex = new ccxt[exchange]({
+		const ex = new ccxt.bybit({
 			apiKey: apikey,
 			secret: secret,
 			enableRateLimit: true,
 			options: {
-				defaultType: "margin",
+				defaultType: "unified",
 			},
 		});
 
 		const params = {
-			marginMode: "cross",
+			isLeverage: 1,
 		};
 
+		await ex.cancelAllOrders();
+
 		let tick = 0;
-		let btcTicker = await ex.fetchTicker(`BTC/${base}`);
-		let feeTicker = await ex.fetchTicker(feeSymbol);
 		while (true) {
 			tick++;
 			try {
@@ -105,33 +98,19 @@ while (true) {
 
 				let midPrice = (ticker.bid + ticker.ask) / 2;
 				let spread = midPrice * spreadPercent;
-				let balanceBase = balance.free[base] || 0;
-				let balanceAsset = balance.free[asset] || 0;
-				let balanceFee = balance.free[fee] || 0;
-
-				if (tick % tickerTicks == 0) {
-					try {
-						btcTicker = await ex.fetchTicker(`BTC/${base}`);
-						feeTicker = await ex.fetchTicker(feeSymbol);
-					} catch (err) {
-						log.red("failed to update btc and fee tickers");
-					}
-				}
-
-				let balanceNetBtc = balance.info.totalNetAssetOfBtc;
-				let netBalance = (balanceNetBtc * btcTicker.bid).toFixed(decimals);
+				let balanceNet = balance.info.result.list[0].totalEquity;
 
 				if (!data?.timestamp || !data?.balance || !data?.symbol) {
 					data.timestamp = Date.now();
-					data.balance = parseFloat(netBalance);
+					data.balance = parseFloat(balanceNet);
 					data.symbol = symbol;
 					await write(data);
 				}
 
-				let profitBalance = (netBalance - data.balance).toFixed(decimals);
-				let profitPercent = (profitBalance / data.balance) * 100;
-				let profitPercentFixed = profitPercent.toFixed(decimals);
-
+				let profitBalance = (balanceNet - data.balance).toFixed(decimals);
+				let profitPercent = ((profitBalance / data.balance) * 100).toFixed(
+					decimals,
+				);
 				let profitAverage = averageProfit(
 					data.timestamp,
 					profitPercent,
@@ -139,28 +118,18 @@ while (true) {
 
 				let orderAmount = ex.amountToPrecision(
 					symbol,
-					(netBalance * ordersAmountPercent) / midPrice,
-				);
-
-				let feeRequired = Math.max(netBalance * feeRequiredPercent, 5.5);
-				let balanceFeeRequired = feeRequired / feeTicker.ask;
-				let balanceBaseRequired = orderAmount * midPrice * 2;
-				let balanceAssetRequired = orderAmount * 2;
-
-				let marginLevel = parseFloat(balance.info.marginLevel).toFixed(
-					decimals,
+					(balanceNet * ordersAmountPercent) / midPrice,
 				);
 
 				let uptime = runtime(data.timestamp);
 				try {
 					if (tick % updateTicks == 0) {
 						let strUptime = ms(uptime);
-						let strStartingBalance = `${data.balance.toFixed(decimals)} ${base}`;
-						let strNetBalance = `${netBalance} ${base}`;
+						let strBalanceNet = `${balanceNet} ${base}`;
+						let strBalanceStart = `${data.balance.toFixed(decimals)} ${base}`;
 						let strProfitBalance = `${profitBalance} ${base}`;
-						let strProfitPercent = `${profitPercentFixed}%`;
+						let strProfitPercent = `${profitPercent}%`;
 						let strProfitAverage = `${profitAverage}%`;
-						let strMarginLevel = `${marginLevel}`;
 
 						if (profitBalance > 0) {
 							strProfitBalance = `+${strProfitBalance}`.green;
@@ -180,25 +149,14 @@ while (true) {
 							strProfitAverage = strProfitAverage.red;
 						}
 
-						if (marginLevel > 20) {
-							strMarginLevel = strMarginLevel.green;
-						} else if (marginLevel > 10) {
-							strMarginLevel = strMarginLevel.cyan;
-						} else if (marginLevel > 3) {
-							strMarginLevel = strMarginLevel.yellow;
-						} else {
-							strMarginLevel = strMarginLevel.red;
-						}
-
 						let tbl = table([
 							{
 								"uptime (human)": strUptime,
-								"balance (start)": strStartingBalance,
-								"balance (net)": strNetBalance,
+								"balance (start)": strBalanceStart,
+								"balance (net)": strBalanceNet,
 								"profit (net)": strProfitBalance,
 								"profit %": strProfitPercent,
 								"profit/day % ": strProfitAverage,
-								"margin level": strMarginLevel,
 							},
 						]);
 						let line = tbl.split("\n")[1];
@@ -210,180 +168,56 @@ while (true) {
 					log.red(err);
 				}
 
-				if (balanceFee < balanceFeeRequired * feeRequiredPercent) {
-					await ex.borrowMargin(base, feeRequired, feeSymbol, params);
-					log.cyan(`borrow ${base} ${feeRequired}`);
-
-					await ex.createMarketBuyOrder(
-						feeSymbol,
-						ex.amountToPrecision(feeSymbol, balanceFeeRequired),
-					);
-					log.cyan(`buy ${fee} ${balanceFeeRequired}`);
-					await new Promise((resolve) => setTimeout(resolve, updateInterval)); // Check every 5 seconds
+				let openOrders = await ex.fetchOpenOrders(symbol);
+				if (openOrders.length > ordersMax) {
 					continue;
 				}
 
-				for (const margin of balance.info.userAssets) {
-					if (margin.asset == fee) {
-						let repayAmountAsset = Math.min(margin.free, margin.interest);
-						if (repayAmountAsset > 0) {
-							try {
-								await ex.repayMargin(
-									margin.asset,
-									repayAmountAsset,
-									symbol,
-									params,
-								);
-								log.green(
-									`paid interest ${margin.asset} ${repayAmountAsset.toFixed(sats)}`,
-								);
-							} catch (err) {}
-						}
-					}
-					if (margin.asset == asset || margin.asset == base) {
-						if (margin.free > 0 && margin.borrowed > 0) {
-							let repayAmountAsset = Math.min(margin.free, margin.borrowed);
-							if (repayAmountAsset > 0) {
-								try {
-									await ex.repayMargin(
-										margin.asset,
-										repayAmountAsset,
-										symbol,
-										params,
-									);
-									log.green(`paid margin ${margin.asset} ${repayAmountAsset}`);
-								} catch (err) {}
-							}
-						}
-					}
-				}
-
-				let buyPrice = ex.priceToPrecision(symbol, midPrice - 1 * spread);
-
-				let sellPrice = ex.priceToPrecision(symbol, midPrice + 1 * spread);
+				await ex.cancelAllOrders();
 
 				let orders = [];
-				let hasSell = false;
-				let hasBuy = false;
-				let openOrders = await ex.fetchOpenOrders(symbol);
-				for (const order of openOrders) {
-					let buyMax = Math.max(order.price, buyPrice);
-					let buyMin = Math.min(order.price, buyPrice);
-					let buyGap = buyMax - buyMin;
-					if (buyGap <= spread) {
-						hasBuy = true;
+				for (let index = 1; index <= ordersMax; index++) {
+					{
+						let side = "buy";
+						let amount = orderAmount;
+						let price = ex.priceToPrecision(symbol, midPrice - index * spread);
+						orders.push({
+							symbol: symbol,
+							side: side,
+							amount: amount,
+							price: price,
+							promise: ex.createLimitOrder(symbol, side, amount, price, params),
+						});
 					}
-					let sellMax = Math.max(order.price, sellPrice);
-					let sellMin = Math.min(order.price, sellPrice);
-					let sellGap = sellMax - sellMin;
-					if (sellGap <= spread) {
-						hasSell = true;
+
+					{
+						let side = "sell";
+						let amount = orderAmount;
+						let price = ex.priceToPrecision(symbol, midPrice + index * spread);
+						orders.push({
+							symbol: symbol,
+							side: side,
+							amount: amount,
+							price: price,
+							promise: ex.createLimitOrder(symbol, side, amount, price, params),
+						});
 					}
 				}
 
-				if (!hasBuy) {
-					if (balanceBase < balanceBaseRequired) {
-						balanceBaseRequired = ex.priceToPrecision(
-							symbol,
-							balanceBaseRequired - balanceBase,
-						);
-
-						if (balanceBaseRequired > 0) {
-							try {
-								await ex.borrowMargin(
-									base,
-									balanceBaseRequired,
-									symbol,
-									params,
-								);
-								log.yellow(`borrow ${base} ${balanceBaseRequired}`);
-							} catch (err) {}
-						}
-					}
-
-					orders.push(
-						ex.createLimitOrder(symbol, "buy", orderAmount, buyPrice, params),
-					);
-				}
-
-				if (!hasSell) {
-					if (balanceAsset < balanceAssetRequired) {
-						balanceAssetRequired = ex.amountToPrecision(
-							symbol,
-							balanceAssetRequired - balanceAsset,
-						);
-						if (balanceAssetRequired > 0) {
-							try {
-								await ex.borrowMargin(
-									asset,
-									balanceAssetRequired,
-									symbol,
-									params,
-								);
-								log.yellow(`borrow ${asset} ${balanceAssetRequired}`);
-							} catch (err) {}
-						}
-					}
-
-					orders.push(
-						ex.createLimitOrder(symbol, "sell", orderAmount, sellPrice),
-					);
-				}
-
-				for (const orderReq of orders) {
+				for (const order of orders) {
 					try {
-						let order = await orderReq;
-						openOrders.push(order);
+						await order.promise;
 						log.green(`create ${order.side} ${order.amount} @ ${order.price}`);
 					} catch (err) {}
 				}
-
-				let minOrder;
-				let maxOrder;
-				let buyCount = 0;
-				let sellCount = 0;
-				for (const order of openOrders) {
-					if (!minOrder || order.price < minOrder.price) {
-						minOrder = order;
-					}
-					if (!maxOrder || order.price > maxOrder.price) {
-						maxOrder = order;
-					}
-
-					if (order.side == "buy") {
-						buyCount++;
-					} else {
-						sellCount++;
-					}
-				}
-
-				if (sellCount > ordersMax) {
-					try {
-						let order = maxOrder;
-						await ex.cancelOrder(order.id, symbol);
-						log.yellow(`cancel ${order.side} ${order.amount} @ ${order.price}`);
-					} catch (err) {
-						log.red(err);
-					}
-				}
-
-				if (buyCount > ordersMax) {
-					try {
-						let order = minOrder;
-						await ex.cancelOrder(order.id, symbol);
-						log.yellow(`cancel ${order.side} ${order.amount} @ ${order.price}`);
-					} catch (err) {
-						log.red(err);
-					}
-				}
 			} catch (err) {
 				log.red(err);
-				await new Promise((resolve) => setTimeout(resolve, failInterval)); // Check every 5 seconds
+				await new Promise((resolve) => setTimeout(resolve, failInterval));
 			}
 		}
 	} catch (err) {
 		log.red(err);
 	}
 
-	await new Promise((resolve) => setTimeout(resolve, failInterval)); // Check every 5 seconds
+	await new Promise((resolve) => setTimeout(resolve, failInterval));
 }
